84d029d14107962fd9cb8a747ff5e4c58ec1eed0
Hedyju <39848031+Hedyju@users.noreply.github.com>
2018-10-30 16:56:02 +0900

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,8 @@
+# 판매 금액, 적립 금액을 계산하여 출력하는 프로그램
+
+price = 50000
+sale_price = price * 0.85
+acc_price = sale_price * 0.03
+print('원가: ', price)
+print('판매가 : ', sale_price)
+print('적립금 : ', acc_price)
